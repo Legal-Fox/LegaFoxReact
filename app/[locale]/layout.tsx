49afbec549cdca69b4ac138d689f0f17587206f3
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-09-22 04:26:06 +0200

    small change favicon

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -36,10 +36,10 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
         <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-          <link rel="manifest" href="/favicon//site.webmanifest" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body className={`${raleway.className} flex flex-col min-h-screen bg-background text-foreground`}>
         <ThemeProvider
