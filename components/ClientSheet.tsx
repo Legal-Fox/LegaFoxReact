ac3b80ae37eccd84e9b4ffec435b1ded69917e08
Anton Malenkij <98691823+AMalenkij@users.noreply.github.com>
2024-09-25 07:39:18 +0200

    When you click on link the mobile menu, close it

---
+++ components/ClientSheet.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import { Menu } from "lucide-react";
+import { usePathname } from 'next/navigation';
+
+import { DialogTitle } from "@/components/ui/dialog";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+
+interface ClientSheetProps {
+  children: React.ReactNode;
+  className?: string;
+  title: string;
+  nameBtn: string;
+}
+
+export default function ClientSheet({ children, className, title, nameBtn }: ClientSheetProps) {
+  const [open, setOpen] = React.useState(false);
+  const pathName = usePathname();
+
+  const toggleOpen = () => setOpen((prevState) => !prevState);
+
+  useEffect(() => {
+    if (open) setOpen(false);
+  }, [pathName]);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button variant="outline" size="icon" className={className} onClick={toggleOpen}>
+          <Menu className="h-6 w-6" />
+          <span className="sr-only">{nameBtn}</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="w-3/4">
+        <DialogTitle className="sr-only">{title}</DialogTitle>
+        {children}
+      </SheetContent>
+    </Sheet>
+  );
+}
--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,14 +1,11 @@
-import { Menu } from "lucide-react";
-
 import ModeToggle from "./ModeToggle";
 import LanguageToggle from './LanguageToggle';
-import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger, SheetTitle } from "@/components/ui/sheet";
 import { Accordion } from "@/components/ui/accordion";
 import { SocialLinks } from './SocialLinks';
 import NavMenu from './NavMenu';
 import { cn } from "@/lib/utils";
 import Logo from "./Logo";
+import ClientSheet from "./ClientSheet";
 
 interface MobileMenuProps {
   tModeToggle: {
@@ -33,40 +30,36 @@ export default function MobileMenu({
   className,
 }: MobileMenuProps) {
   return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="outline" size="icon" className={cn("md:hidden", className)}>
-          <Menu className="h-6 w-6" />
-          <span className="sr-only">Toggle menu</span>
-        </Button>
-      </SheetTrigger>
-      <SheetContent side="left" className="w-3/4">
-        <nav className="flex flex-col space-y-4">
-        <SheetTitle><Logo variant="dialog"/></SheetTitle>
-          <Accordion type="single" collapsible className="w-full">
-            <LanguageToggle
-              variant="accordion"
-              englishLabel={tLanguageToggle.english}
-              polishLabel={tLanguageToggle.polski}
-              ukrainianLabel={tLanguageToggle.ukrainian}
-              russianLabel={tLanguageToggle.russian}
-              changeLanguageLabel={tLanguageToggle.changeLanguage}
-            />
-            <ModeToggle 
-              variant="accordion"
-              lightLabel={tModeToggle.lightLabel}
-              darkLabel={tModeToggle.darkLabel}
-              systemLabel={tModeToggle.systemLabel}
-              toggleTheme={tModeToggle.toggleTheme}
-            />
-          </Accordion>
-          <NavMenu variant='default' className='flex flex-col items-start'/>
-        </nav>
-        <SocialLinks 
-          variant="sidebar"  
-          className="flex flex-col justify-center items-center absolute bottom-10 right-10"
-        />
-      </SheetContent>
-    </Sheet>
+    <ClientSheet 
+      className={cn("md:hidden", className)} 
+      title="Mobile Menu" 
+      nameBtn="Toggle menu"
+    >
+      <nav className="flex flex-col space-y-4">
+        <Logo variant="dialog" />
+        <Accordion type="single" collapsible className="w-full">
+          <LanguageToggle
+            variant="accordion"
+            englishLabel={tLanguageToggle.english}
+            polishLabel={tLanguageToggle.polski}
+            ukrainianLabel={tLanguageToggle.ukrainian}
+            russianLabel={tLanguageToggle.russian}
+            changeLanguageLabel={tLanguageToggle.changeLanguage}
+          />
+          <ModeToggle
+            variant="accordion"
+            lightLabel={tModeToggle.lightLabel}
+            darkLabel={tModeToggle.darkLabel}
+            systemLabel={tModeToggle.systemLabel}
+            toggleTheme={tModeToggle.toggleTheme}
+          />
+        </Accordion>
+        <NavMenu variant='default' className='flex flex-col items-start' />
+      </nav>
+      <SocialLinks
+        variant="sidebar"
+        className="flex flex-col justify-center items-center absolute bottom-10 right-10"
+      />
+    </ClientSheet>
   );
 }
